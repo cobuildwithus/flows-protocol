3677cbbb7e9a74aa724f33307089fc69273c0608
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-01-13 16:24:14 -0500

    deploy upgrade script

---
+++ script/DeployERC20Upgrade.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.28;
+
+import { DeployScript } from "./DeployScript.s.sol";
+import { ERC20VotesMintable } from "../src/ERC20VotesMintable.sol";
+
+contract DeployERC20MintableUpgrade is DeployScript {
+    address public mintableImplementation;
+
+    function deploy() internal override {
+        // Deploy new ERC20VotesMintable implementation
+        ERC20VotesMintable mintableImpl = new ERC20VotesMintable();
+        mintableImplementation = address(mintableImpl);
+    }
+
+    function writeAdditionalDeploymentDetails(string memory filePath) internal override {
+        vm.writeLine(filePath, string(abi.encodePacked("New MintableImpl: ", addressToString(mintableImplementation))));
+    }
+
+    function getContractName() internal pure override returns (string memory) {
+        return "ERC20VotesMintable.Upgrade";
+    }
+}

f143937d6bc144ba8016b4b472b20aef2898e360
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-01-13 17:47:45 -0500

    storage layout checks (#66)
    * storage layout checks
    
    * fix
    
    * try dis
    
    * minor fix
    
    * Update test.yml
    
    * try to add secret
    
    * Update storage-check.sh
    
    * Update test.yml
    
    * update new storage layout

---
+++ script/storage-check.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -e
+
+generate() {
+  file=$1
+  if [[ $func == "generate" ]]; then
+    echo "Creating storage layout diagrams for the following contracts from $contracts_file"
+  fi
+
+  echo "=======================" > "$file"
+  echo "👁 STORAGE LAYOUT snapshot 👁" >"$file"
+  echo "=======================" >> "$file"
+
+  # Read contracts from file, removing @ prefix if present
+  contracts_list=$(cat "${contracts_file#@}")
+  
+  # Log the contracts we're checking
+  echo "Checking contracts:"
+  echo "$contracts_list"
+  echo "..."
+  
+  for contract in $contracts_list
+  do
+    { echo -e "\n======================="; echo "➡ $contract" ; echo -e "=======================\n"; } >> "$file"
+    FOUNDRY_PROFILE=default forge inspect --pretty "$contract" storage-layout >> "$file"
+  done
+  if [[ $func == "generate" ]]; then
+    echo "Storage layout snapshot stored at $file"
+  fi
+}
+
+if ! command -v forge &> /dev/null
+then
+    echo "forge could not be found. Please install forge by running:"
+    echo "curl -L https://foundry.paradigm.xyz | bash"
+    exit
+fi
+
+func=$1
+contracts_file=$2
+filename=.storage-layout
+new_filename=.storage-layout.temp
+
+if [[ $func == "check" ]]; then
+  generate $new_filename
+  if ! cmp -s .storage-layout $new_filename ; then
+    echo "storage-layout test: fails ❌"
+    echo "The following lines are different:"
+    diff -a --suppress-common-lines "$filename" "$new_filename"
+    rm $new_filename
+    exit 1
+  else
+    echo "storage-layout test: passes ✅"
+    rm $new_filename
+    exit 0
+  fi
+elif [[ $func == "generate" ]]; then
+  generate "$filename"
+else
+  echo "unknown command. Use 'generate' or 'check' as the first argument."
+  exit 1
+fi

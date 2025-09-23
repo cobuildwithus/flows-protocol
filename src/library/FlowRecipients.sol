a15db08c2ff9cb563d636c59cdf51f22e9566abf
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-09-22 23:02:48 -0400

    Revert "Update FlowRecipients.sol"
    This reverts commit 7fb61ee732417f7f710102ec8e88bc701cc8b0e7.

--- src/library/FlowRecipients.sol
+++ src/library/FlowRecipients.sol
@@ -66,8 +66,7 @@ library FlowRecipients {
 
         if (recipient == address(0)) revert IFlow.ADDRESS_ZERO();
         if (fs.recipientExists[recipient]) revert IFlow.RECIPIENT_ALREADY_EXISTS();
-        FlowTypes.FlowRecipient storage prev = fs.recipients[recipientId];
-        if (prev.recipient != address(0) && !prev.removed) revert IFlow.RECIPIENT_ALREADY_EXISTS();
+        if (fs.recipients[recipientId].recipient != address(0)) revert IFlow.RECIPIENT_ALREADY_EXISTS();
 
         fs.recipientExists[recipient] = true;
 
@@ -98,8 +97,7 @@ library FlowRecipients {
         FlowTypes.RecipientMetadata memory metadata
     ) public returns (bytes32) {
         if (fs.recipientExists[recipient]) revert IFlow.RECIPIENT_ALREADY_EXISTS();
-        FlowTypes.FlowRecipient storage prev = fs.recipients[recipientId];
-        if (prev.recipient != address(0) && !prev.removed) revert IFlow.RECIPIENT_ALREADY_EXISTS();
+        if (fs.recipients[recipientId].recipient != address(0)) revert IFlow.RECIPIENT_ALREADY_EXISTS();
 
         fs.recipients[recipientId] = FlowTypes.FlowRecipient({
             recipientType: FlowTypes.RecipientType.FlowContract,
